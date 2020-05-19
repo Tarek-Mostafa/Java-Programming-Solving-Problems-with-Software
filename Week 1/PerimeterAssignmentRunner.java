db3207181e21f5180dc13a7f6fb6055276950c19
Mukil57 <65532886+Mukil57@users.noreply.github.com>
2020-05-19 15:27:31 +0530

    Update PerimeterAssignmentRunner.java

--- Week 1/PerimeterAssignmentRunner.java
+++ Week 1/PerimeterAssignmentRunner.java
@@ -147,6 +147,6 @@ public class PerimeterAssignmentRunner {
 
     public static void main (String[] args) {
         PerimeterAssignmentRunner pr = new PerimeterAssignmentRunner();
-        pr.testPerimeter();
+       
     }
 }
